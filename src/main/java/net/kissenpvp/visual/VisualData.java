50f61440de9c6c0fe3e0b516659146787c53d6d4
Ivo Quiring <falk.quiring@gmail.com>
2025-07-28 16:41:13 +0200

    Add `@NotNull` annotations and refactor `VisualManager` methods

--- src/main/java/net/kissenpvp/visual/VisualData.java
+++ src/main/java/net/kissenpvp/visual/VisualData.java
@@ -1,5 +1,6 @@
 package net.kissenpvp.visual;
 
 import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.NotNull;
 
-public record VisualData(Component prefix, Component suffix, int priority, Component header, Component footer) {}
+public record VisualData(@NotNull Component prefix, @NotNull Component suffix, int priority, @NotNull Component header, @NotNull Component footer) {}
--- src/main/java/net/kissenpvp/visual/VisualManager.java
+++ src/main/java/net/kissenpvp/visual/VisualManager.java
@@ -5,21 +5,26 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 public class VisualManager {
+
     private final Map<UUID, Team> teams = new HashMap<>();
     private final Scoreboard board = Bukkit.getScoreboardManager().getMainScoreboard();
 
-    public void update(Player player, VisualData data) {
+    public void update(@NotNull Player player, @NotNull VisualData data) {
         setHeaderFooter(player, data.header(), data.footer());
-        setTeam(player, data.prefix(), data.suffix(), data.priority());
+        setTeam(player, data);
     }
 
-    public void remove(Player player) {
+    public void remove(@NotNull Player player) {
         Team team = teams.remove(player.getUniqueId());
         if (team != null) {
             team.removeEntry(player.getName());
@@ -27,22 +32,32 @@ public class VisualManager {
         }
     }
 
-    private void setHeaderFooter(Player player, Component header, Component footer) {
+    private void setHeaderFooter(@NotNull Player player, @NotNull Component header, @NotNull Component footer) {
         player.sendPlayerListHeader(header);
         player.sendPlayerListFooter(footer);
     }
 
-    private void setTeam(Player player, Component prefix, Component suffix, int priority) {
+    private void setTeam(@NotNull Player player, @NotNull VisualData data) {
+
         Team oldTeam = teams.remove(player.getUniqueId());
-        if (oldTeam != null) oldTeam.unregister();
+        if (oldTeam != null)
+        {
+            oldTeam.unregister();
+        }
 
-        String teamName = String.format("%03d_%s", priority, player.getName().substring(0, Math.min(10, player.getName().length())));
+        String name = player.getName().substring(0, Math.min(10, player.getName().length()));
+        String teamName = String.format("%03d_%s", data.priority(), name);
+
         Team team = board.getTeam(teamName);
-        if (team == null) team = board.registerNewTeam(teamName);
+        if (Objects.isNull(team))
+        {
+            team = board.registerNewTeam(teamName);
+        }
 
-        team.prefix(prefix);
-        team.suffix(suffix);
+        team.prefix(data.prefix());
+        team.suffix(data.suffix());
         team.addEntry(player.getName());
+
         teams.put(player.getUniqueId(), team);
         player.setScoreboard(board);
     }
